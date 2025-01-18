e808edcb9c6a9f53e65d12a647e73eef1ec3574c
Kavi Arya <kaviarya047@tutamail.com>
2025-01-18 11:00:30 +0530

    general updates

--- lua/user/vimtex.lua
+++ lua/user/vimtex.lua
@@ -4,7 +4,7 @@ local M = {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = "sioyek"
+    vim.g.vimtex_view_method = "zathura"
   end
 }
 
